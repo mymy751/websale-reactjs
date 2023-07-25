3ffbaf153b7de44d4e72f1ce8307716e5564c54a
DiemMy <ntdmy751@gmail.com>
2023-07-25 12:45:09 +0700

    Init reactjs

---
+++ fe/mycode-reactjs/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <h1>Học Hành Chăm Chỉ</h1>
+  <HomePage/>
+  <div>
+    <h1>Học</h1>
+  </div>
+);
